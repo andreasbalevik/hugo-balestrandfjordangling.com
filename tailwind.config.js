2a58cc0ae824a9aa48663225366619114c692555
Andreas Balevik <andreas.balevik@nav.no>
2026-02-27 09:14:53 +0100

    bump

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          text: '#1f2c3d',
+        },
+      },
+    },
+  },
+}
